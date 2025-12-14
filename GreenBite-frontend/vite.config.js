1ff811cc4573d57a3bd6d68d1e121f6927b635b4
Elsayed-Elpna <said.elpna@yahoo.com>
2025-12-14 18:50:55 +0200

    add @ pack

--- GreenBite-frontend/vite.config.js
+++ GreenBite-frontend/vite.config.js
@@ -2,8 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
