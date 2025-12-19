432facfd977f5b367dd71be5397b14dabaa81ab3
Elsayed-Elpna <said.elpna@yahoo.com>
2025-12-19 21:18:08 +0200

    WIP: foodlog filters backend + login updates

--- GreenBite-frontend/src/components/LoginPage/LoginForm.jsx
+++ GreenBite-frontend/src/components/LoginPage/LoginForm.jsx
@@ -3,12 +3,49 @@ import { Label, TextInput, Button, Checkbox } from 'flowbite-react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 export default function LoginForm() {
-    const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [formData, setFromData] = useState({
+    email: "",
+    password: "",
+  });
+  const [formError, setFormError] = useState();
+  const [fieldErros, setFieldErrors] = useState({
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('Login Successfully');
+  const validateForm = () => {
+    let errors = {
+      email: "",
+      password: "",
     };
+    if (!emailRegex.test(formData.email)) {
+      errors.email = "Please enter a valid email address";
+    }
+    if (!passwordRegex.test(formData.password)) {
+      errors.password = "Password cannot be empty or contain spaces";
+    }
+    setFieldErrors(errors);
+    return !errors.email && !errors.password;
+  };
+
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    const isValid = validateForm();
+    if (!isValid) return;
+    try {
+      await login(formData);
+      console.log("login succse");
+      navigate("/home");
+    } catch (error) {
+      console.log(`login failed ${error}`);
+      setFormError("Invalid email or password ");
+    }
+  };
 
     return (
         <div className="w-full max-w-xl lg:max-w-3xl">
@@ -93,25 +130,27 @@ export default function LoginForm() {
                     </a>
                 </div>
 
-                {/* Submit Button */}
-                <Button
-                    type="submit"
-                    onClick={handleSubmit}
-                    className="w-full h-11 lg:h-12 text-sm lg:text-base font-semibold mt-5 cursor-pointer"
-                    color="green"
-                    outline
-                >
-                    Login
-                </Button>
-
-                {/* Login Link */}
-                <p className="text-center text-sm lg:text-base text-gray-700 pt-1">
-                    Don't have an account?{' '}
-                    <a href="#" className="text-green-500 hover:underline font-medium">
-                        Sign Up
-                    </a>
-                </p>
-            </div>
-        </div>
-    );
+        {/* Submit Button */}
+        <Button
+          type="submit"
+          className="w-full h-11 lg:h-12 text-sm lg:text-base font-semibold cursor-pointer select-none "
+          color="green"
+          outline
+          disabled={loading || !formData.email || !formData.password}
+        >
+          Login
+        </Button>
+        {/* Signup Link */}
+        <p className="text-center text-sm lg:text-base text-gray-700 pt-1">
+          Don't have an account?{" "}
+          <Link
+            to="/register"
+            className="text-green-500 hover:underline font-medium"
+          >
+            Sign Up
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 }
